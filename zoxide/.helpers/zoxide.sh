d64b409554375c38bff167a96875432db9df864a
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-09-13 20:31:14 -0500

    feat(zoxide): add zinit function for managing zoxide directories

---
+++ zoxide/.helpers/zoxide.sh
@@ -0,0 +1,15 @@
+#!/usr/bin/env bash
+
+zinit() {
+    zoxide query --list | while read -r line; do
+        zoxide remove "$line"
+    done
+
+    find "$HOME/code" -mindepth 1 -maxdepth 1 -type d | while read -r dir; do
+        find "$dir" -mindepth 1 -maxdepth 1 -type d | while read -r subdir; do
+            zoxide add "$subdir"
+        done
+    done
+
+    zoxide add "$HOME/dotfiles"
+}
