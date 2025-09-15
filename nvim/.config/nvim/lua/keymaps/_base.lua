782d575c7d819ecfb7e909ed3f7dd463df0a0bc0
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-09-14 19:34:46 -0500

    feat(nvim): add support for multiple escape key combinations in keymap

--- nvim/.config/nvim/lua/keymaps/_base.lua
+++ nvim/.config/nvim/lua/keymaps/_base.lua
@@ -1,5 +1,10 @@
-vim.keymap.set({ "i", "v", "x" }, "jk", "<Esc>", {
-    noremap = true,
-    silent = true,
-    desc = "Exit to Normal mode",
-})
+local esc_keys = { "jk", "kj" }
+
+-- esc keys shjortcut to exit insert/visual modes
+for _, key in ipairs(esc_keys) do
+    vim.keymap.set({ "i", "v", "x" }, key, "<Esc>", {
+        noremap = true,
+        silent = true,
+        desc = "Exit to Normal mode",
+    })
+end
