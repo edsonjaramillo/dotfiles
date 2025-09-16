c009ea7f57c6984bd739c7a38242e2d927861d1d
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-09-16 13:49:11 -0500

    feat(nvim): move configuration to lualine statusline plugin

--- nvim/.config/nvim/lua/plugins/statusline.lua
+++ nvim/.config/nvim/lua/plugins/statusline.lua
@@ -1,4 +1,19 @@
 return {
     "nvim-lualine/lualine.nvim",
     dependencies = { "nvim-tree/nvim-web-devicons" },
+    config = function()
+        local earthshine_lualine_theme = require("lualine.themes.earthshine")
+        require("lualine").setup({
+            options = {
+                theme = earthshine_lualine_theme,
+            },
+            sections = {
+                lualine_a = { "mode" },
+                lualine_b = { "branch", "diff" },
+                lualine_c = { "diagnostics" },
+                lualine_x = { "filename", "encoding", "fileformat", "filetype", "lsp_status" },
+                lualine_y = { "windows" },
+            },
+        })
+    end,
 }
