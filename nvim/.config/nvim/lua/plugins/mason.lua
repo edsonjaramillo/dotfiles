800999fa783d8882cc8cadf0b48eaa930238c55a
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-09-25 11:54:43 -0500

    feat(lazy): move to lazy and update plugins init

---
+++ nvim/.config/nvim/lua/plugins/mason.lua
@@ -0,0 +1,41 @@
+return {
+    -- lspconfig
+    {
+        "mason-org/mason.nvim",
+        build = ":MasonUpdate",
+        opts = {},
+        config = function()
+            require("mason").setup()
+            -- Enable virtual text for diagnostics
+            vim.diagnostic.config({ virtual_text = true })
+        end,
+    },
+    {
+        "WhoIsSethDaniel/mason-tool-installer.nvim",
+        dependencies = { "mason-org/mason.nvim" },
+        config = function()
+            require("mason-tool-installer").setup({
+                ensure_installed = {
+                    -- lua
+                    "lua-language-server",
+                    "stylua",
+                    -- bash
+                    "bash-language-server",
+                    "shfmt",
+                    -- "shellcheck",
+                    -- node
+                    -- "ts_ls",
+                    -- "eslint",
+                    -- "prettier",
+                    -- python
+                    -- "pyright",
+                    -- "ruff",
+                    -- json
+                    -- "jsonls",
+                    -- toml
+                    -- "tombi",
+                },
+            })
+        end,
+    },
+}
