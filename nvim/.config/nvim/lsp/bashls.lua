0936abf089080471bdc90b73b17729b577e8f924
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-09-26 14:13:58 -0500

    feat(lsp): remove bashls and lua_ls configuration files

--- nvim/.config/nvim/lsp/bashls.lua
+++
@@ -1,7 +0,0 @@
-return {
-	cmd = { "bash-language-server", "start" },
-	filetypes = { "sh", "bash" },
-	root_markers = { ".git/", ".bashrc", ".bash_profile", ".profile" },
-	single_file_support = true,
-	log_level = vim.lsp.protocol.MessageType.Warning,
-}
