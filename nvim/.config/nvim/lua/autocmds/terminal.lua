839ff2d12590e95150f39435b24a5b5f23af2993
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-09-29 14:06:05 -0500

    feat(terminal): add terminal keymaps and toggleterm plugin configuration

---
+++ nvim/.config/nvim/lua/autocmds/terminal.lua
@@ -0,0 +1,14 @@
+local esc_keys = { "jk", "kj" }
+local function set_terminal_keymaps()
+	local opts = { buffer = 0 }
+	vim.keymap.set("t", "<esc>", [[<C-\><C-n>]], opts)
+	for _, key in ipairs(esc_keys) do
+		vim.keymap.set("t", key, [[<C-\><C-n>]], opts)
+	end
+end
+
+-- when entering a terminal, set the keymaps
+vim.api.nvim_create_autocmd("TermOpen", {
+	pattern = [[term://*]],
+	callback = set_terminal_keymaps,
+})
