786bc47d45b016a0f6f9b2f0571e58c2add33a3a
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-09-30 14:04:12 -0500

    feat(focus): add focus.nvim plugin with configuration for autoresize and UI settings

--- nvim/.config/nvim/lua/plugins/qol.lua
+++ nvim/.config/nvim/lua/plugins/qol.lua
@@ -12,4 +12,22 @@ return {
 	{
 		"windwp/nvim-ts-autotag",
 	},
+	{
+		"nvim-focus/focus.nvim",
+		version = "*",
+		config = function()
+			require("focus").setup({
+				autoresize = {
+					enable = false,
+				},
+				split = {
+					bufnew = true,
+				},
+				ui = {
+					relativenumber = true,
+					winhighlight = false,
+				},
+			})
+		end,
+	},
 }
