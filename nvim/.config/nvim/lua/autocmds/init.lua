1c227a6251437957dc8ab64620342dae697dcbff
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-09-30 21:24:17 -0500

    feat(qol): add autocmds for buffer management and cursor position restoration

--- nvim/.config/nvim/lua/autocmds/init.lua
+++ nvim/.config/nvim/lua/autocmds/init.lua
@@ -1,7 +1,8 @@
 -- Auto commands configuration for formattter
--- require("autocmds.format.prettier")
+require("autocmds.format.prettier")
 require("autocmds.format.shfmt")
 require("autocmds.format.stylua")
 
 require("autocmds.terminal")
 require("autocmds.auto-source")
+require("autocmds.qol")
