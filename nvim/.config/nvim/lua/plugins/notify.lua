fa2e1aa35738d27cfdb452861afc5ad392c3830b
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-09-30 21:21:44 -0500

    feat(notify): enhance notification options with time format configuration

--- nvim/.config/nvim/lua/plugins/notify.lua
+++ nvim/.config/nvim/lua/plugins/notify.lua
@@ -1,6 +1,12 @@
-return { {
-	"rcarriga/nvim-notify",
-	opts = {
-		background_colour = "#000000",
+return {
+	{
+		"rcarriga/nvim-notify",
+		opts = {
+			background_colour = "#000000",
+			time_formats = {
+				notification = "%-I:%M:%S %p",
+			},
+			timeout = 1000,
+		},
 	},
-} }
+}
