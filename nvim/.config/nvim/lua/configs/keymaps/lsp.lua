6dce3d412e39e39c2529b49b10c7793eb640de1a
Edson Jaramillo <edsonajaramillo@gmail.com>
2025-10-04 19:43:59 -0500

    refactor(lsp/keymaps): remove snacks-specific LSP mappings
    - Remove <leader>lc mapping for code actions.
    - Remove <leader>li mapping that invoked
    snacks.picker.lsp_implementations.
    - Remove <leader>lS mapping that invoked
    snacks.picker.lsp_workspace_symbols.
    
    Why:
    - Reduce reliance on snacks-specific pickers in the core LSP keymap
    file.
    - Avoid duplicate or conflicting mappings and simplify LSP keybindings
    so built-in handlers or alternative pickers can be used consistently.

--- nvim/.config/nvim/lua/configs/keymaps/lsp.lua
+++ nvim/.config/nvim/lua/configs/keymaps/lsp.lua
@@ -20,30 +20,11 @@ wk.add({
 		desc = "Rename (LSP)",
 	},
 	{
-		"<leader>lc",
-		vim.lsp.buf.code_action,
-		desc = "Code Action (LSP)",
-	},
-	{
-		"<leader>li",
-		function()
-			snacks.picker.lsp_implementations()
-		end,
-		desc = "Go to Implementation (Snacks)",
-	},
-	{
 		"<leader>lR",
 		function()
 			snacks.picker.lsp_references()
 		end,
 		desc = "References (Snacks)",
-	},
-	{
-		"<leader>lS",
-		function()
-			snacks.picker.lsp_workspace_symbols()
-		end,
-		desc = "Workspace Symbols (Snacks)",
 	},
 	{
 		"K",
